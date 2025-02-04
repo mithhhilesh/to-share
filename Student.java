b36db929c965d9edc70401b00ff3198f89e0f3b2
Mithlesh Yeole <mithlesh.yeole27@gmail.com>
2025-02-04 09:56:58 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,24 +2,21 @@ import java.util.Scanner;
 public class Student {
     int roll;
     String name;
-//    void display(){
-//        System.out.println();
-//        System.out.println();
-//
-//    }
     Student(int roll, String name){
         this.roll=roll;
         this.name=name;
-        System.out.println(roll);
-        System.out.println(name);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Student arr[];
         arr = new Student[3];
-        arr[0] = new Student(18, "Mith");
-
+        arr[0] = new Student(18, "Saad");
+        arr[1] = new Student(19, "Saad ka bhai");
+        arr[2] = new Student(121, "Shizuka");
+        for(int i=0; i<3; i++){
+            System.out.println("name:"+ arr[i].name);
+            System.out.println("roll no.: "+ arr[i].roll);
+        }
     }
-
 }
